75a43951b13f91e458131b2c86677e48e2027dc7
Steven K. Sharp <steven.k.sharp@gmail.com>
2019-02-21 19:13:16 -0800

    Stack project outline.

---
+++ project4/edu/sdccd/mesa/cisc190/util/Calculator.java
@@ -0,0 +1,26 @@
+package edu.sdccd.mesa.cisc190.util; 
+
+public class IntegerStack
+{
+    // member variables go here
+
+    public Stack()
+    {
+    }
+
+    public Stack(int capacity)
+    {
+    }
+
+    public int peek()
+    {
+    }
+
+    public int pop()
+    {
+    }
+
+    public void push()
+    {
+    }
+}
