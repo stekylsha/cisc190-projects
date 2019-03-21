125feca3816438c1acfc346c1d793feead81c995
Steven K. Sharp <steven.k.sharp@gmail.com>
2019-03-21 01:35:15 -0700

    Miscellaneous changes to the IntegerStack.

--- project4/edu/sdccd/mesa/cisc190/util/IntegerStack.java
+++ project4/edu/sdccd/mesa/cisc190/util/IntegerStack.java
@@ -3,26 +3,66 @@ package edu.sdccd.mesa.cisc190.util;
 public class IntegerStack
 {
     // member variables go here
+    private int[] intArray;
+    private int arrayPosition;
 
     public IntegerStack()
     {
+        intArray = new int[10];
+        arrayPosition = 0;
     }
 
-    public IntegerStack(int capacity)
+    public IntegerStack(int initialCapacity)
     {
+        intArray = new int[initialCapacity];
+        arrayPosition = 0;
     }
 
+    /**
+     * Look at the top value in the intArray, without removing it.
+     * @return The value at the top.
+     */
     public int peek()
     {
-        return 0;
+        return intArray[arrayPosition-1];
     }
 
+    /**
+     * Pop a value from the intArray.
+     * @return The value removed.
+     */
     public int pop()
     {
-        return 0;
+        if (arrayPosition <= 0) {
+            arrayPosition -= 1;
+            return intArray[arrayPosition];
+        }
+        else
+        {
+            // fixme: don't return if there's no value
+            return 0;
+        }
     }
 
-    public void push()
+    /**
+     * Push a value into the intArray.
+     * @param value The value to push.
+     */
+    public void push(int value)
     {
+        if (arrayPosition < intArray.length)
+        {
+            intArray[arrayPosition] = value;
+            arrayPosition += 1;
+        }
+        else
+        {
+            // do something nasty
+        }
+    }
+    
+    public String toString()
+    {
+        return "{ arrayPosition : " + arrayPosition + ", intArray : " + intArray.toString() + " }";
     }
 }
